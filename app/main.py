097ec80ee12e481a795720dc8dac7340d10fbe33
unknown <hammadzuri@gmail.com>
2026-07-28 20:40:12 +0100

    .env created and implemented

--- app/main.py
+++ app/main.py
@@ -1,39 +1,19 @@
-# ==========================================================
-# IMPORTS
-# ==========================================================
-# Optional allows a field to be optional (can be None)
-# from typing import Optional
 from pydantic import BaseModel, Field
-
-# Import the FastAPI class used to create the application
 from fastapi import FastAPI
-
-# Import classes used for error handling and responses
 from fastapi import HTTPException, Response, status,Depends
-
-# BaseModel is used to create request body models
-# Field is used to add validation rules
-
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from . import  models, auth
-
 import time
-# from sqlalchemy.orm import Session
-# from sqlalchemy.exc import IntegrityError
-# from . import models, schemas, utils 
-# from .schemas import PostCreate, PostBase, PostResponse
-# from . import schemas
-
 from .routers import post, users, profile
-
 from .database import engine, get_db
+
+
+
+
 
 models.Base.metadata.create_all(bind=engine)
 
-# ==========================================================
-# CREATE FASTAPI APPLICATION
-# ==========================================================
 
 # Create an instance of FastAPI
 app = FastAPI(
@@ -59,33 +39,3 @@ while True:
         print ("Error: ", error) 
         time.sleep(5)  # Wait for 5 seconds before retrying
 
-# ==========================================================
-# PYDANTIC MODELS
-# ==========================================================
-
-# Base model containing the common fields for a post.
-# This model is reused for both creating and updating posts.
-#imported from schemas.py
-#imported Post from schemas.py
-
-# ==========================================================
-# HOME ROUTE
-# ==========================================================
-
-# GET /
-#
-# This is the root endpoint.
-#
-# Visiting:
-#
-# http://127.0.0.1:8000/
-#
-# returns the message below.
-
-@app.get("/")
-def root():
-
-    return {
-        "message": "Welcome to Wenyfour FastAPI App",
-        "status": "success",
-    }
