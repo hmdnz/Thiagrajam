0b7d5b93906957b5d9d44b1579a567a1fff08389
unknown <hammadzuri@gmail.com>
2026-07-31 15:17:30 +0100

    xyz

--- app/auth.py
+++ app/auth.py
@@ -31,3 +31,37 @@ def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
     access_token = oauth2.create_access_token(data={"user_id": db_user.id})
 
     return {"access_token": access_token, "token_type": "bearer"}
+
+# @router.post("/login")
+# def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
+
+#     db_user = db.query(models.User).filter(
+#         or_(
+#             models.User.email == user.email,
+#             models.User.phone_number == user.phone_number
+#         )
+#     ).first()
+
+#     print("Login email:", user.email)
+#     print("User found:", db_user)
+
+#     if not db_user:
+#         raise HTTPException(status_code=401, detail="Invalid email or password")
+
+#     print("Stored hash:", db_user.password)
+
+#     result = utils.verify(user.password, db_user.password)
+
+#     print("Password match:", result)
+
+#     if not result:
+#         raise HTTPException(status_code=401, detail="Invalid email or password")
+
+#     access_token = oauth2.create_access_token(
+#         data={"user_id": db_user.id}
+#     )
+
+#     return {
+#         "access_token": access_token,
+#         "token_type": "bearer"
+#     }
