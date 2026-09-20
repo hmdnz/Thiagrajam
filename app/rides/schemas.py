8224f0ba8d1d24a19c55759d26804c0d3db69ac2
unknown <hammadzuri@gmail.com>
2026-09-20 19:55:21 +0100

    removedestination and origin states

--- app/rides/schemas.py
+++ app/rides/schemas.py
@@ -198,9 +198,9 @@ class RideCreate(BaseModel):
 
     # Macro Journey (City/State level)
     origin_city: str
-    origin_state: Optional[str] = None
+    
     destination_city: str
-    destination_state: Optional[str] = None
+   
 
     # Micro Meeting Points
     pickup_location: str
